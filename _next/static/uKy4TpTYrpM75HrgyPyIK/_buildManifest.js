961d633d553f219ea4a4566403b144266f70821d
sriram1701 <lovelysriram1712@gmail.com>
2026-09-17 19:59:49 +0530

    deploy: build to gh-pages

---
+++ _next/static/uKy4TpTYrpM75HrgyPyIK/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sri-web-squad//_next/:path+",
+        "destination": "/sri-web-squad/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
